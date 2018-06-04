ce005471282221d4584bea512acbf1f64161bc25
Bernardo Tolosa <bernardotolosajr@gmail.com>
2018-06-04 15:32:20 +0800

    refactor

--- app/service/firebase.js
+++ app/service/firebase.js
@@ -17,55 +17,63 @@ class Firebase extends Service {
     const token = await this._getAccessToken()
     const { url } = this.config.firebase
 
-    async.waterfall([
-      function(callback) {
-        const data = r.table('configs').run().then((result) => {
-          const { id } = result[0]
-          callback(null, id)
-        })
-      },
-      function(id, callback) {
-        const { parameters } = params
-        r.table('configs').get(id).update({ parameters }).run().then((result) => {
-          callback(null, id)
-        })
-      },
-      function(id, callback) {
-        r.table('configs').get(id).run().then((result) => {
-          callback(null, result)
-        })
-      },
-      function(config, callback) {
-        const { id, etag, parameters } = config
-        axios({
-          method: 'put',
-          url: url,
-          headers: {
-            Authorization: 'Bearer ' + token,
-            'Content-Type': 'application/json; UTF-8',
-            'Accept-Encoding': 'gzip',
-            'If-Match': etag
-          },
-          data: { parameters }
-        }).then((response) => {
-          if (response.status === 200) {
-            const etag = response.headers['etag']
-            // update new etag
+    return new Promise((resolve, reject) => {
+      async.waterfall([
+        function(callback) {
+          const data = r.table('configs').run().then((result) => {
+            const { id } = result[0]
+            callback(null, id)
+          })
+        },
+        function(id, callback) {
+          const { parameters } = params
+          r.table('configs').get(id).update({ parameters }).run().then((result) => {
+            callback(null, id)
+          })
+        },
+        function(id, callback) {
+          r.table('configs').get(id).run().then((result) => {
+            callback(null, result)
+          })
+        },
+        function(config, callback) {
+          const { id, etag, parameters } = config
+          axios({
+            method: 'put',
+            url: url,
+            headers: {
+              Authorization: 'Bearer ' + token,
+              'Content-Type': 'application/json; UTF-8',
+              'Accept-Encoding': 'gzip',
+              'If-Match': etag
+            },
+            data: { parameters }
+          }).then((response) => {
+            if (response.status === 200) {
+              const etag = response.headers['etag']
+              callback(null, id, etag)
+            } else {
+              callback(null)
+            }
+          })
+        },
+        function(id, etag, callback) {
+          if (etag) {
             r.table('configs').get(id).update({ etag }).run().then((result) => {
               callback(null, true)
             })
           } else {
             callback(null, false)
           }
-        })
-      }
-    ], function(err, result) {
-      if (err) {
-        console.log(err)
-        return
-      }
-      console.log('success')
-    });
+        }
+      ], function(error, result) {
+        if (error) {
+          rereject({ error: error })
+          return
+        }
+        resolve({ success: result })
+      });
+    })
   }
 
   async getConfigs() {
@@ -84,33 +92,36 @@ class Firebase extends Service {
             }
           }).then((response) => {
             const etag = response.headers['etag']
-            const { parameters } = response.data
+            const data = response.data
 
-            const data = {
-              etag: etag,
-              parameters,
-            }
-
-            r.table('configs').count().run().then((count) => {
-              if (count) {
-                r.table('configs').run().then((configs) => {
-                  const id = configs[0].id;
-                  r.table('configs').get(id).update(data).run().then((result) => {
-                    callback(null, true)
-                  })
-                });
-              } else {
-                r.table('configs').insert(response.data).run().then((result) => {
-                  callback(null, true)
-                }).catch((error) => {
-                  callback(null, false)
-                });
-              }
-            });
+            callback(null, etag, data)
           })
         },
+        function(etag, data, callback) {
+          r.table('configs').count().run().then((count) => {
+            if (count) {
+              const { parameters } = data
+              r.table('configs').run().then((configs) => {
+                const id = configs[0].id;
+                r.table('configs').get(id).update({
+                  etag,
+                  parameters
+                }).run().then((result) => {
+                  callback(null, true)
+                })
+              });
+            } else {
+              r.table('configs').insert(data).run().then((result) => {
+                callback(null, true)
+              }).catch((error) => {
+                callback(null, false)
+              });
+            }
+          });
+        },
         function (args, callback) {
-          r.table('configs').run().then((result) => { const parameter = result[0].parameters;
+          r.table('configs').run().then((result) => {
+            const parameter = result[0].parameters;
             const c = Object.keys(parameter).map(key => {
               const obj = {};
               obj[key] = parameter[key]
